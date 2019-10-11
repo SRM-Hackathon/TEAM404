565b72c1cc8e3b062aa6cc47fa40c99d517de554
unknown <sarjhur@gmail.com>
2019-10-11 20:43:42 +0530

    Added spinner and finished profile fragment

--- app/src/main/java/com/example/shoppersbuddy/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/shoppersbuddy/ui/home/HomeFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,15 +18,46 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.shoppersbuddy.R;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private HomeViewModel homeViewModel;
+    Spinner spinner;
+   // View v;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        spinner= root.findViewById(R.id.spinner);
+        spinner.setPrompt("Category");
+        // Spinner Drop down elements
+        ArrayList<String> categories = new ArrayList<String>();
+        categories.add("Food");
+        categories.add("Clothing");
+        categories.add("Entertainment");
+        categories.add("Electronics");
+        categories.add("Cosmetics");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+
 
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +65,23 @@ public class HomeFragment extends Fragment {
 
             }
         });
+
+        spinner.setOnItemSelectedListener(this);
         return root;
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String item = adapterView.getItemAtPosition(i).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
